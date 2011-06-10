79e2c5d539c7ba91d37810fbe3400d3c688ac71d
Travis Reeder <travis@appoxy.com>
2011-06-10 13:20:20 -0700

    rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempRails::Application.config.secret_token = '3a33b3cf65330954159905e36eec6a3f34835f5999f98778cb45e989c1a05d382f88b45a19da6775d8e3d083be57be57a0837e43de58a140fb910ee09e29b056'
